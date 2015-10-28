6e7a5b2dd4ccb6bf70cdd7f9654893b4bd06202b
maxxou65 <maxxou65@hotmail.fr>
2015-10-28 16:17:21 +0100

    Création de la classe BufWrit

---
+++ BufWrit.java
@@ -0,0 +1,13 @@
+package com.test.cours;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BufWrit {
+
+
+}
