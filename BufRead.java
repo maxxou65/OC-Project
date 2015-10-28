815027033f0f1958ee9b33394ae0d6a310bef39b
maxxou65 <maxxou65@hotmail.fr>
2015-10-28 16:15:49 +0100

    Création de la classe BufRead

---
+++ BufRead.java
@@ -0,0 +1,12 @@
+package com.test.cours;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufRead {
+
+
+
+}
